31e2a0ccc21aff3d242280bbad7ac5bcf7cf30a0
Deepika Aratakatla <53089236+deepika-aratakatla@users.noreply.github.com>
2026-09-01 14:55:20 +0530

    Implement taxonomy status check for campaign names
    Added a query to check the validity of campaign names.

--- sql/taxonomy_audit.sql
+++ sql/taxonomy_audit.sql
@@ -1,1 +1,11 @@
-SELECT 'Taxonomy Audit';
+SELECT
+    campaign_name,
+    CASE
+        WHEN REGEXP_CONTAINS(
+            campaign_name,
+            r'^[A-Za-z0-9]+(_[A-Za-z0-9]+){7}$'
+        )
+        THEN 'VALID'
+        ELSE 'INVALID'
+    END AS taxonomy_status
+FROM campaign_master;
