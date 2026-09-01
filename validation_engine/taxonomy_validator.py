968cc3d704d8e9d02f96036ac370cb1ffd124020
Deepika Aratakatla <53089236+deepika-aratakatla@users.noreply.github.com>
2026-09-01 14:54:49 +0530

    Implement campaign taxonomy validation using pandas

--- validation_engine/taxonomy_validator.py
+++ validation_engine/taxonomy_validator.py
@@ -1,1 +1,11 @@
-print("Campaign Taxonomy Validator")
+ import pandas as pd
+
+df = pd.read_csv("../sample_data/campaign_metadata.csv")
+
+for campaign in df["campaign_name"]:
+    parts = campaign.split("_")
+
+    if len(parts) == 8:
+        print(f"{campaign}: VALID")
+    else:
+        print(f"{campaign}: INVALID")
